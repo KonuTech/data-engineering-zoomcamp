cf90048409877486800867cdf36947bb75768274
KonuTech <borowiec.k@gmail.com>
2024-01-14 20:24:34 +0100

    dockerized postgresql

--- 01-docker-terraform/2_docker_sql/pipeline.py
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -1,11 +1,5 @@
 import sys
 
-import pandas as pd
-
-print(sys.argv)
 
 day = sys.argv[1]
-
-# some fancy stuff with pandas
-
-print(f'job finished successfully for day = {day}')
+print(f"TEST: job finished successfully for day = {day}")
